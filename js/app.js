c35224eef9b54c95f1a194b9e000a0391ec83438
oussama <oussamadabachilpro@gmail.com>
2022-01-05 14:25:35 +0100

    js working

--- js/app.js
+++ js/app.js
@@ -8,21 +8,25 @@ let mail_value = document.querySelector("#Mypwd");
 
 let verif_change_text = document.querySelector(".verif_text")
 
-let submmit_btn = document.querySelector(".subconnect")
+let submmit_btn = document.querySelector(".sub_connect")
 
 
-submmit_btn.addEventListener("click",()=>{
+submmit_btn.addEventListener("click",(e)=>{
     alert(password_value.value)
     alert(mail_value.value)
-    confirm("hey")
+    e.preventDefault();
 
 
     if((password_data==password_value.value)&&(user_data==mail_value.value)){
-        window.open("./login_page.html")        }else
+        confirm("good")
+        location.href = "../login_page.html"; 
+            
+    }else
         {       
+            confirm("not good")
 
 
-            verif_change_text.classList.toggle("verif_text_change")
+            verif_change_text.classList.toggle("verif_text_show")
             verif_change_text.innerText= "Votre adresse mail et votre mot de passe sont incorrects "
 
         }
