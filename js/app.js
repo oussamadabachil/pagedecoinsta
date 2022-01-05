ab734218df6874d15d6e1fefdfa1b4ca7725bf9e
oussama <oussamadabachilpro@gmail.com>
2022-01-05 15:13:57 +0100

    stop alert and add icon and respinsive mobike

--- js/app.js
+++ js/app.js
@@ -12,21 +12,18 @@ let submmit_btn = document.querySelector(".sub_connect")
 
 
 submmit_btn.addEventListener("click",(e)=>{
-    alert(password_value.value)
-    alert(mail_value.value)
     e.preventDefault();
 
 
     if((password_data==password_value.value)&&(user_data==mail_value.value)){
-        confirm("good")
         location.href = "../login_page.html"; 
-            
+        verif_change_text.classList.remove("verif_text_show")
+ 
     }else
         {       
-            confirm("not good")
 
 
-            verif_change_text.classList.toggle("verif_text_show")
+            verif_change_text.classList.add("verif_text_show")
             verif_change_text.innerText= "Votre adresse mail et votre mot de passe sont incorrects "
 
         }
